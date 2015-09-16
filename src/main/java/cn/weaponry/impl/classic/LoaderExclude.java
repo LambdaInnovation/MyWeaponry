affc709b9aadf3ce4a6da5830c172106d2d49e50
WeAthFolD <cheetah77@qq.com>
2015-09-16 17:14:21 +0800

    Start over again

--- src/main/java/cn/weaponry/impl/classic/LoaderExclude.java
+++
@@ -1,9 +0,0 @@
-package cn.weaponry.impl.classic;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-@interface LoaderExclude {
-
-}
